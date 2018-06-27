346bf3643bd3b37c40a0f98f16152006b2d23dcd
IfraA <ifraa.aijaz@gmail.com>
2018-06-26 18:34:58 -0700

    homepg

--- client/src/Component/Cards/Cards.js
+++ client/src/Component/Cards/Cards.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Cards.css";
 
+
+// handleClick = event => {
+//     const { name, image } = event.target;
+//     this.setState({
+//         [name]: image
+//     });
+// }
 const Cards = props => (
     <div className="card" onClick={() => props.handleClick(props.index)}>
 
