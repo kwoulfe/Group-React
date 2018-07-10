f947566d9c2ee99a38b2a2014ef975edc6b1e16c
IfraA <ifraa.aijaz@gmail.com>
2018-07-09 21:57:56 -0700

    comment

--- client/src/Components/CommentWindow/CommentWindow.js
+++ client/src/Components/CommentWindow/CommentWindow.js
@@ -8,21 +8,24 @@ class CommentWindow extends React.Component {
     render = () => {
         return (
 
-            <div className="container">
+            <div className="container-com">
                 <h1>Comments</h1>
+                <br />
                 {this.props.comments.map(comment => (
-                    <div className="commentBox"
-                        id={comment.id}
-                        key={comment.id}
-                        author={comment.author}
-                        text={comment.text}
-                        date={comment.date}>
-                        <div className="row">
-                            <h3>{comment.author}</h3>
-                            <h3>{comment.date}</h3>
-                        </div>
-                        <div className="row">
-                            <h4>{comment.text}</h4>
+                    <div>
+                        <div className="commentBox"
+                            id={comment.id}
+                            key={comment.id}
+                            author={comment.author}
+                            text={comment.text}
+                            date={comment.date}>
+                            <div className="row">
+                                <h5>{comment.author}</h5>
+                                <h5>{comment.date}</h5>
+                            </div>
+                            <div className="row">
+                                <p>{comment.text}</p>
+                            </div>
                         </div>
                     </div>
                 ))}
