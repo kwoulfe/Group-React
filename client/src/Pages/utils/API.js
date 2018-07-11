127f825abd66fdb3ef6e2cc679771f651572d8c6
Kevin Woulfe <kevinpwoulfe@gmail.com>
2018-07-11 15:27:11 -0700

    missing the 'n'

--- client/src/Pages/utils/API.js
+++ client/src/Pages/utils/API.js
@@ -18,7 +18,7 @@ export default {
 
   // Comment API's
   createPost: function(data) {
-    return axios.post('/api/commets/', data);
+    return axios.post('/api/comments/', data);
   },
 
   getPosts: function() {
