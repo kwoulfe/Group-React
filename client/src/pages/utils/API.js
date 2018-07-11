508ac56d91fbffcc2ebca3fabd9acbf9cbd420d0
Kevin Woulfe <kevinpwoulfe@gmail.com>
2018-07-11 14:31:57 -0700

    API

--- client/src/pages/utils/API.js
+++ client/src/pages/utils/API.js
@@ -2,37 +2,28 @@ import axios from 'axios';
 
 export default {
   // User API's
-  getUsers: function() {
+  getUsers: function () {
     return axios.get('/api/users');
   },
-  getUser: function(id) {
+  getUser: function (id) {
     console.log('~~~~getuser serverside was called~~~~');
     return axios.get('/api/users/' + id);
   },
-  updateDonation: function(id, data) {
+  updateDonation: function (id, data) {
     return axios.put('/api/users/' + id, data);
   },
-  newUser: function(data) {
+  newUser: function (data) {
     return axios.post('/api/users/', data);
   },
 
   // Comment API's
 
-  createPost: function(data) {
-    return axios.post('/api/comments/', data);
+  createPost: function (data) {
+    return axios.post("/api/comments/", data)
   },
 
-  getPosts: function(projectId) {
+  getPosts: function (projectId) {
     return axios.get(`/api/comments/${projectId}`);
-  }
 
-  // post route contingent on logged in.
-  // Deletes the book with the given id
-  //   deleteBook: function(id) {
-  //     return axios.delete("/api/books/" + id);
-  //   },
-  //   // Saves a book to the database
-  //   saveBook: function(bookData) {
-  //     return axios.post("/api/books", bookData);
-  //   }
+  }
 };
